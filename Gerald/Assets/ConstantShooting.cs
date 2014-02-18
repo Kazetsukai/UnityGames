b3bdce78bf513786cfab49f4e2d38d0dc3c742af
Kazetsukai <moeruze.burning@gmail.com>
2014-02-18 23:03:43 +1300

    First version
    You can move around and stuff

---
+++ Gerald/Assets/ConstantShooting.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConstantShooting : MonoBehaviour {
+
+	float timePassed = 0;
+
+	public GameObject Bullet;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timePassed += Time.fixedDeltaTime;
+		if (timePassed > 0.1f)
+		{
+			timePassed -= 0.1f;
+			var bullet1 = (GameObject)Instantiate(Bullet);
+			var bullet2 = (GameObject)Instantiate(Bullet);
+			
+			bullet1.transform.position = transform.position + new Vector3(-0.2f, 0.2f, 0);
+			bullet2.transform.position = transform.position + new Vector3(0.2f, 0.2f, 0);
+		}
+	}
+}
